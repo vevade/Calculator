09a37a4e3ce1ce59bb5ea69c092f4038ed52428e
vevade <bochavlada@gmail.com>
2026-07-11 14:46:12 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,31 @@
+print("Примечание! Пишите ваше выражение с пробелами, иначе программа выдаст ошибку")
+while True:
+    we = input("Введите выражение(или 'exit' для выхода):" )
+    if we == "exit":
+        break
+
+    try:
+        a, sign, b = we.split()
+        a = float(a)
+        b = float(b)
+
+        if sign == "+":
+            print(a+b)
+        elif sign == "-":
+            print(a-b)
+        elif sign == "*":
+            print(a*b)
+        elif sign == "/":
+            if b == 0:
+                print("Ошибка деления")
+            else:
+                print(a/b)
+        else:
+            print("Неподдерживаемая операция. Используйте +,-,*,/")
+
+
+    except ValueError:
+        print("Ошибка: введите выражение в формате 'число знак число'")
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+
